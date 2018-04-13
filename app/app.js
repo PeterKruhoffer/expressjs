367b76278ff97f84112c2d6a5f7d80e483656c3d
PeterKruhoffer <Peter.kruhoffer@gmail.com>
2018-04-13 15:44:55 +0200

    jas

--- app/app.js
+++ app/app.js
@@ -4,7 +4,16 @@ var dataFile = require('./data/data.json');
 
 app.set('port', process.env.PORT || 3000 );
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res) 
+{
+  res.send(`
+      <h1>Welcome</h1>
+      <p>Roux Academy Meetups put together artist from all walks of life</p>
+  `);
+});
+
+
+app.get('/speakers', function(req, res) {
   var info = '';
   dataFile.speakers.forEach(function(item) {
     info += `
@@ -19,6 +28,20 @@ app.get('/', function(req, res) {
       ${info}
   `);
 });
+
+
+app.get('/speakers/:speakerid', function(req, res) {
+  
+  var speaker = dataFile.speakers[req.params.speakerid];
+  res.send(`
+      <h1>${speaker.title}</h1>
+      <h2>with${speaker.name}</h2>
+      <p>${speaker.summary}</p>
+  `);
+});
+
+
+
 
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
